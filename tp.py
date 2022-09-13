84ae6dae0619587b50fe0681c9132263d11a7756
Alexis <alexisjrojas@hotmail.es>
2022-09-13 20:10:40 -0300

    Menu

--- tp.py
+++ tp.py
@@ -1,6 +1,7 @@
 import os 
 import pickle
 import os.path
+clear = lambda x: os.system(x)
 
 class Operaciones():
     def __init__(self) -> None:
@@ -60,13 +61,68 @@ afr="./rubros.dat"
 afrxp="./rubrosxproductos.dat"
 afs="./silos.dat"
 
-
 alo=crear_archivos(afo)
 alp=crear_archivos(afp)
 alr=crear_archivos(afr)
 alrxp=crear_archivos(afrxp)
 als=crear_archivos(afs)
-        
 
 
+def menu_visible():
+    print("[1] ADMINISTRACIONES \n[2] ENTREGA DE CUPOS \n[3] RECEPCION \n[4] REGISTRAR CALIDAD \n[5] REGISTRAR PESO BRUTO[6] REGISTRAR DESCARGA\n[7] REGISTRAR TARA\n[8] REPORTES\n[0] Fin del programa")
+
+
+def menu():
+    opc="-1"
+    while opc != "0":
+        clear("cls")
+        menu_visible()
+        opc=input("")
+        match opc:
+            case "1":
+                print("hola")
+            case "2":
+                print("hola")
+            case "3":
+                print("hola")
+            case "4":
+                print("hola")
+            case "5":
+                print("hola")
+            case "6":
+                print("hola")
+            case "7":
+                print("hola")
+            case "8":
+                print("hola")
+            case "9":
+                print("hola")
+            case "0":
+                print("Has salido")
+            case _:
+                pass
+menu()
+
+
+
+
+# lang = input("What's the programming language you want to learn? ")
+
+# match lang:
+#     case "1":
+#         ("You can become a web developer.")
+
+#     case "2":
+#         ("You can become a Data Scientist")
+
+#     case "3":
+#         ("You can become a backend developer")
+    
+#     case "4":
+#         ("You can become a Blockchain developer")
+
+#     case "5":
+#         ("You can become a mobile app developer")
+#     case _:
+#         ("The language doesn't matter, what matters is solving problems.")
 

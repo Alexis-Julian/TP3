109124cbbe1dd9c0c11807366b48e2b4b6482ca9
Alexis <alexisjrojas@hotmail.es>
2022-09-15 22:06:11 -0300

    Facha

--- tp.py
+++ tp.py
@@ -34,11 +34,6 @@ class Silos():
         self.codpro=0
         self.stock=0
 
-class comp():
-    def __init__(self) -> None:
-        self.var1=0
-        self.var2=""
-
 """ ---------------------------DEF---------------------- """
         
 def validarpatente(opc):
@@ -57,13 +52,43 @@ def validar_tipo(tipo,opc,desde,hasta):
     except:
         return True
 
+def formatear(obj,b):
+    if b==0:
+        obj.cod=str(obj.cod)
+        obj.cod=obj.cod.ljust(10," ")
+        obj.nombre=obj.nombre.ljust(20," ")
+
+def busqueda_secuencial(al,af,instancia,cod):
+    bus=instancia()
+    noencontrado=True
+    al.seek(0)
+    t=os.path.getsize(af)
+    while al.tell() <t and noencontrado != False:
+        bus=pickle.load(al)
+        if int(bus.cod) == int(cod):
+            noencontrado=False
+            pos=al.tell()
+        else:
+            pos=-1
+    return pos
+
+def mostrar(vr,archf,archl):
+    t=os.path.getsize(archf)
+    archl.seek(0)
+    if t== 0:
+        print("No hay nda")
+    else:
+        while archl.tell() < t:
+            vr=pickle.load(archl)
+            print(vr.cod,vr.nombre)
+
 def crear_archivos(archivo):
     if not os.path.exists(archivo):
         logico=open(archivo,"w+b")
     else:
         logico=open(archivo,"r+b")
     return logico
-
+#-----------------------Archivos
 afo="./operaciones.dat"
 afp="./productos.dat"
 afr="./rubros.dat"
@@ -75,7 +100,6 @@ alp=crear_archivos(afp)
 alr=crear_archivos(afr)
 alrxp=crear_archivos(afrxp)
 als=crear_archivos(afs)
-
 #----------------------Procedures1
 
 def construccion():
@@ -90,23 +114,8 @@ def submenu_administacion_visible():
 
 def menu_visible():
     print("[1] ADMINISTRACIONES \n[2] ENTREGA DE CUPOS \n[3] RECEPCION \n[4] REGISTRAR CALIDAD \n[5] REGISTRAR PESO BRUTO\n[6] REGISTRAR DESCARGA\n[7] REGISTRAR TARA\n[8] REPORTES\n[0] Fin del programa")
+
 #---------------------Functions
-
-def formatear(obj,b):
-    if b==0:
-        obj.cod=str(obj.cod)
-        obj.cod=obj.cod.ljust(10," ")
-        obj.nombre=obj.nombre.ljust(20," ")
-
-def mostrar(vr,archf,archl):
-    t=os.path.getsize(archf)
-    archl.seek(0)
-    if t== 0:
-        print("No hay nda")
-    else:
-        while archl.tell() < t:
-            vr=pickle.load(archl)
-            print(vr.cod,vr.nombre)
 
 def alta_producto_rubro(car,nombre,al,af):
     clear("cls")
@@ -141,7 +150,51 @@ def alta_producto_rubro(car,nombre,al,af):
     mostrar(car,af,al)
     clear("pause")
 
+def alta_silos_rubroxproducto(af):
+    t=os.path.getsize(af)
 
+def submenu_administacion(opc1):
+    opc=""
+    while opc != "V":
+        clear("cls")
+        submenu_administacion_visible()
+        opc=input("").upper()
+        match opc:
+            case "A":
+                altas(opc1)
+            case "B":
+                print("Hola")
+            case "C":
+                print("Hola")
+            case "M":
+                print("Hola")
+            case "V":
+                pass 
+            case _:
+                print("Opcion invalida")
+                                
+def altas(opc1):
+    if opc1 == "B":
+        car=Productos() #Define donde estas yendo######################
+        alta_producto_rubro(car,"Prodcutos",alp,afp)
+    elif opc1== "C":
+        rub=Rubros() 
+        alta_producto_rubro(rub,"rubro",alr,afr)
+    
+def administraciones():
+    opc=""
+    while opc != "V":
+        clear("cls")
+        adminstracion_visible()
+        opc=input("").upper()
+        if opc == "B"  or opc == "C" or opc== "D" or opc == "E":
+            submenu_administacion(opc)
+        elif opc == "A" or opc =="F" or opc=="G":
+            construccion()            
+        elif opc != "V":
+            print("Opcion invalida")
+            clear("pause")
+  
 def menu():
     opc=""
     while opc != "0":
@@ -172,66 +225,6 @@ def menu():
             case _:
                 pass
 
-def administraciones():
-    opc=""
-    while opc != "V":
-        clear("cls")
-        adminstracion_visible()
-        opc=input("").upper()
-        if opc == "B"  or opc == "C" or opc== "D" or opc == "E":
-            submenu_administacion(opc)
-        elif opc == "A" or opc =="F" or opc=="G":
-            construccion()            
-        elif opc != "V":
-            print("Opcion invalida")
-            clear("pause")
-        
-
-def submenu_administacion(opc1):
-    opc=""
-    while opc != "V":
-        clear("cls")
-        submenu_administacion_visible()
-        opc=input("").upper()
-        match opc:
-            case "A":
-                altas(opc1)
-            case "B":
-                print("Hola")
-            case "C":
-                print("Hola")
-            case "M":
-                print("Hola")
-            case "V":
-                pass 
-            case _:
-                print("Opcion invalida")
-                    
-            
-
-def altas(opc1):
-    if opc1 == "B":
-        car=Productos() #Define donde estas yendo######################
-        alta_producto_rubro(car,"Prodcutos",alp,afp)
-    elif opc1== "C":
-        rub=Rubros() 
-        alta_producto_rubro(rub,"rubro",alr,afr)
-    
-
-  
-def busqueda_secuencial(al,af,instancia,cod):
-    bus=instancia()
-    noencontrado=True
-    al.seek(0)
-    t=os.path.getsize(af)
-    while al.tell() <t and noencontrado != False:
-        bus=pickle.load(al)
-        if int(bus.cod) == int(cod):
-            noencontrado=False
-            pos=al.tell()
-        else:
-            pos=-1
-    return pos
             
 
 menu()
